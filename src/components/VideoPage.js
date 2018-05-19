6c959e8ecad2528e4acea720f428c888e9029812
unknown <S_Gravesen92@hotmail.com>
2018-05-19 18:42:18 +0200

    Added page-header class

--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -7,13 +7,13 @@ import VideoList from './VideoList';
 const VideoPage = (props) => {
     return (
         <div className="VideoPage">
-            <h1>
-                <SearchFrom getVideos={props.getVideos}/>
-                <div className="video-screen">
-                    <VideoDetails videoDetails={props.videoDetails}/>
-                    <VideoList videos={props.videos}/>
-                </div>
-            </h1>
+        <div className="page-header">
+            <SearchFrom getVideos={props.getVideos}/>
+        </div>
+            <div className="video-screen">
+                <VideoDetails videoDetails={props.videoDetails}/>
+                <VideoList videos={props.videos}/>
+            </div>
         </div>
     );
 };
