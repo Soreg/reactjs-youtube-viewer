8e876eb7fbea9fc831064ea7721e38eb2dac59ff
unknown <S_Gravesen92@hotmail.com>
2018-05-19 18:28:59 +0200

    VideoListItems now running from foreach (map)

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import VideoListItem from './VideoListItem';
 
 const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem video={video} key={video.id.videoId}/>
+    });
     return (
         <ul className="VideoList">
-            <VideoListItem />
+            {videoItems}
         </ul>
     );
 }
